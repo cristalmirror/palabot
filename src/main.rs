12796ccd411da2eec7b5322dbb32b90ade80e997
cristalmirror <crisrojar@gmail.com>
2026-02-08 15:48:59 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use teloxide::prelude::*;
+use teloxide::utils::command::BotCommands;
+
+
+//enum of the commands list and compiler settings
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "Commandos disponibles:")]
+enum Commands {
+    #[description("Buscar informacionen Google... ")]
+    Buscarengoogle(String),
+    #[description("Muestra el cumpleaños de un usuario... ")]
+    Cumpleaños(String),
+    #[description("Silencia a un usuario por una hora (Solo Admins). ")]
+    Bloqueo(String),
+}
+
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+//Structure to own DB json
+type Db Arc<Mutex<HashMap<String,String>>>;
+
+//main function with tokio traits
+#[tokio::main]
+async fn main() {
+    let bot = Bot::from_env();
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+
+    //charger the data base JSON (simple)
+
+    let handler = Update::filter_message()
+        .filter_command::<Commands>
+        .endpoint(answer);
+    
+    Dispatcher::builder(bot, handler)
+        .dependecies(dptree::desp![db])
+        .build()
+        .dispatch()
+        .await;
+
+}
+
+async fn answer(
+    bot: Bot,
+    msg: Message, 
+    cmd: Commands, 
+    db: Db,) -> ResponseResult<()> {
+    
+    match cmd {
+        Commands::Buscarengoogle(query) => {
+            //here integer the logic of find extern
+            bot.send_message(msg.chat.id, format!("Resultado de Busquedas... \n {query}")).await;
+        }
+        commands::Cumpleaños(mencion) => {
+            let data = db.lock().await;
+            //find the name in the json charger [8,9]
+            let respuesta = data.get(&mencion)
+                .map(|fecha| format!("el cumpleaños de {mencion} es el {fecha}"))
+                .unwrap_or_else(|| "Usuarios no encontrado en la base de datos.".to_string());
+            bot.send_message(msg.chat.id, respuesta).await;
+        }
+        Commands::Bloqueo(mencion) => {
+            //La logica de bloqueo requiere verificar permisos de admin
+            //Teloxide maneja estas peticiones a la API de telegram [4]
+            bot.send_message(msg.chat.id, format!("Usuarios {mencion} silenciado por 1 hora.")).await?;
+        }
+    }
+    Ok(())
+}
